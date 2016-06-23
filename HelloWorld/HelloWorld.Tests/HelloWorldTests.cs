91252a46a6aef633e0104d98f617b0f288ced3ad
rmoo771 <ron.m771@gmail.com>
2016-06-23 17:36:45 -0500

    second commit

---
+++ HelloWorld/HelloWorld.Tests/HelloWorldTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+       
+       
+        [TestMethod]
+        public void ConsoleWriteLine_OutputText_ExpectHelloWorld()
+        {
+
+            // Arrange
+            var mocKConsole = new Mock<IOutputWriter>();           
+            var consoleWriter= new ConsoleWriter();
+            consoleWriter.Console = mocKConsole.Object;
+            
+            // Act
+            consoleWriter.Console.WriteLine("Hello World");
+
+            // Assert
+            mocKConsole.Verify(console => console.WriteLine("Hello World"));     
+           
+
+        }
+    }
+}
